a815ae99dfec4b494904e4dcccbbe69060ace9d5
taehee_Jung <palzmnbvcx@gmail.com>
2022-12-07 11:56:57 +0900

    [추가] 모집하기 페이지  게시판 페이지네이션 기능 추가

--- static/js/join.js
+++ static/js/join.js
@@ -2,3 +2,27 @@
     function openPopup(){
         window.open("http://127.0.0.1:5500/templates/create_content.html", "new", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=500, height=500, left=700, top=250" );
     }
+
+    // 페이지네이션
+    // 페이지네이션
+    // 페이지네이션
+
+    // jQuery Plugin: http://flaviusmatis.github.io/simplePagination.js/
+
+    var items = $(".pagination_wrapper .pagination_item");
+    var numItems = items.length;
+    var perPage = 5;
+
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "&laquo;",
+        nextText: "&raquo;",
+        onPageClick: function (pageNumber) {
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+        }
+    });
