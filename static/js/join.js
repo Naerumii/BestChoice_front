1760fe0c5ce5df6e814841a18cdd9a20d8a8d07b
taehee_Jung <palzmnbvcx@gmail.com>
2022-12-07 11:55:41 +0900

    [추가] 게시글 작성하기 팝업창기능 추가

---
+++ static/js/join.js
@@ -0,0 +1,4 @@
+    // 팝업창
+    function openPopup(){
+        window.open("http://127.0.0.1:5500/templates/create_content.html", "new", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=500, height=500, left=700, top=250" );
+    }
