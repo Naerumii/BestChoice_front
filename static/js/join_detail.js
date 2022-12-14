52df1e98270561f391a830c813637a79bf145df5
yoojun <leehk77789@gmail.com>
2022-12-14 21:12:19 +0900

    [수정] 로그제거

--- static/js/join_detail.js
+++ static/js/join_detail.js
@@ -4,18 +4,13 @@ const join_article_id = urlParams.get("join_article_id");
 
 // 특정 게시물 back에서 받아오는 함수
 async function getArticleDetail(join_article_id) {
-    console.log(12)
-    console.log(12)
-    console.log(join_article_id)
     const response = await fetch(`http://127.0.0.1:8000/articles/festival/join/${join_article_id}/`, {
         headers: {
             Authorization: "Bearer " + localStorage.getItem("access"),
         },
         method: "GET",
     });
-    console.log(join_article_id)
     response_json = await response.json();
-    console.log(response_json);
     // 받아온 값을 json화 시키고 콘솔로그 확인
     // getArticleDetail() 안에 article_id 써주고, article_detail.js에도 getArticleDetail(article_id);실행
   
@@ -25,7 +20,6 @@ async function getArticleDetail(join_article_id) {
 // back에서 받아온 json 데이터 front에 내용 붙이는 함수
 async function loadDetailArticles(join_article_id) {
     article = await getArticleDetail(join_article_id);
-console.log(12)
     //프론트엔드에서 태그 id 확인하기
     const festival = document.getElementById("join_detail_festival");
     const title = document.getElementById("join_detail_title");
@@ -33,7 +27,6 @@ console.log(12)
     const period = document.getElementById("join_detail_period");
     const count = document.getElementById("join_detail_count");
     // const recruit = document.getElementById("join_detail");
-    console.log(1333)
     festival.innerText = "축제명 : " +article.join_festival;
     title.innerText = "제목 : " +article.join_title;
     desc.innerText = article.join_desc;
