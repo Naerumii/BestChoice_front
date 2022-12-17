ae2079d5b65c63ebeb9f24a817eb50b94bcb7d7d
unknown <winkite1226@gmail.com>
2022-12-18 03:54:09 +0900

    [추가] 모집게시글 댓글 작성자를 통한 버튼 활성화 기능 추가

--- static/js/join_detail.js
+++ static/js/join_detail.js
@@ -5,9 +5,8 @@ const user_id = parseJwt("access").user_id;
 // back에서 받아온 json 데이터 front에 내용 붙이는 함수
 async function loadDetailArticles(join_article_id) {
   article = await getJoinDetail(join_article_id);
-  nowuser = await getProfile(user_id)
-  console.log(nowuser)
-  console.log(article)
+  const nowuser = await getProfile(user_id)
+  
   //프론트엔드에서 태그 id 확인하기
   const festival = document.getElementById("join_detail_festival");
   const title = document.getElementById("join_detail_title");
@@ -48,15 +47,17 @@ async function loadDetailArticles(join_article_id) {
         article.comments[i].comment_user,
         article.comments[i].comment_content,
         article.comments[i].comment_created_at,
-        article.comments[i].id
+        article.comments[i].id,
+        nowuser.user_nickname
       );
     }
   }
 }
 
 
-function get_join_comment_html(user, comment, created_at, id) {
-  temp_html = `<li class="flex-box">
+function get_join_comment_html(user, comment, created_at, id, nickname) {
+  if (user==nickname) {
+    temp_html = `<li class="flex-box">
                         <div class="user-text">
                             <p id="join_comment_user">${user}</p>
                             <div id="join_comment_box_${id}">
@@ -70,7 +71,20 @@ function get_join_comment_html(user, comment, created_at, id) {
                             <a href="#" id="join_update_button_${id}" type="button" onclick="editJoinCommentEvent(${id})">수정</a>
                             <a href="#" type="button" onclick="deleteJoinComment(${id})">삭제</a>
                         </div>
-                        </li>`;
+                  </li>`;
+  } else {
+    temp_html = `<li class="flex-box">
+                        <div class="user-text">
+                            <p id="join_comment_user">${user}</p>
+                            <div id="join_comment_box_${id}">
+                                <p id="join_comment_${id}">${comment}</p>
+                            </div>
+                            <small id="join_date" class="gray-text">${
+                              created_at.split("T")[0]
+                            } ${created_at.split("T")[1].split(".")[0]}</small>
+                        </div>
+                  </li>`;
+  }
   $("#comment_box").append(temp_html);
 }
 
