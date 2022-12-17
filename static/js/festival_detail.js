8ba6df00ddd221979dec4fc9e4e80965628aa636
unknown <winkite1226@gmail.com>
2022-12-17 23:39:15 +0900

    [수정] 북마크 오류 수정

--- static/js/festival_detail.js
+++ static/js/festival_detail.js
@@ -5,7 +5,7 @@ const festival_article_id = urlParams.get("festival_article_id");
 // back에서 받아온 json 데이터 front에 내용 붙이는 함수
 async function loadDetailArticles(festival_article_id) {
     article = await getFestivalDetail(festival_article_id);
-    console.log(article)
+
     //프론트엔드에서 태그 id 확인하기
     const title = document.getElementById("festival_title");
     const desc = document.getElementById("festival_desc");
@@ -29,38 +29,22 @@ async function loadDetailArticles(festival_article_id) {
    
     $('#abc').append(temp_html)
 
-    let bookmarks = article.bookmarks
-    // 북마크
+    // 현재 사용자가 누구인지
     let now_user_id = parseJwt('access').user_id
-    console.log(now_user_id)
 
-    let article_id = `${festival_article_id}`
-    article_id = parseInt(article_id.slice(0, 3))
+    // 현재 축제게시글에 대한 북마크 정보
+    let bookmarks = article.bookmarks
 
-    if (bookmarks.length > 1) {
-        let bookmark_user_id = `${bookmarks[bookmarks.length-1].bookmark_user}`
-        bookmark_user_id = parseInt(bookmark_user_id.slice(0, 3))
-        let bookmark_article_id = `${bookmarks[bookmarks.length-1].bookmark_festival}`
-        bookmark_article_id = parseInt(bookmark_article_id.slice(0, 3))
-        if (now_user_id == bookmark_user_id && article_id == bookmark_article_id) {
-            // $(".클래스 이름").attr("class","변경 할 클래스명");
-            $(`.bookmark${festival_article_id}`).css("color", "#FA5882");
-            $(`.bookmark${festival_article_id}`).addClass("fa-bookmark");
-            $(`.bookmark${festival_article_id}`).removeClass("fa-bookmark-o");
-        }
-    } else if (bookmarks.length == 1) {
-        let bookmark_user_id = `${bookmarks[0].bookmark_user}`
-        bookmark_user_id = parseInt(bookmark_user_id.slice(0, 3))
-        let bookmark_article_id = `${bookmarks[0].bookmark_festival}`
-        bookmark_article_id = parseInt(bookmark_article_id.slice(0, 3))
-        if (now_user_id == bookmark_user_id && article_id == bookmark_article_id) {
-            // $(".클래스 이름").attr("class","변경 할 클래스명");
-            $(`.bookmark${festival_article_id}`).css("color", "#FA5882");
-            $(`.bookmark${festival_article_id}`).addClass("fa-bookmark");
-            $(`.bookmark${festival_article_id}`).removeClass("fa-bookmark-o");
+    if (bookmarks.length > 0) {
+        for(let i=0; i <bookmarks.length; i++) {
+            if(now_user_id == bookmarks[i].bookmark_user) {
+                $(`.bookmark${festival_article_id}`).css("color", "#FA5882");
+                $(`.bookmark${festival_article_id}`).addClass("fa-bookmark");
+                $(`.bookmark${festival_article_id}`).removeClass("fa-bookmark-o");
+                break;
+            }
         }
     }
-    
 }
 
 
